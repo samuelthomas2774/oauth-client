cb27ffaa461a82258c36ca4ff752c0e871ec21d2
Samuel Elliott <samuelthomas2774@outlook.com>
2014-12-06 13:25:39 +0000

    Create google.class.php

---
+++ src/google.class.php
@@ -0,0 +1,25 @@
+<?php
+	/* class OAuthGoogle
+	 * /src/google.class.php
+	 */
+	require_once 'oauth.class.php';
+	
+	class OAuthGoogle extends OAuth {
+		// Options. These shouldn't be modified here, but using the OAuth::options() function.
+		public $options = Array(
+			"session_prefix"		=> "google_",
+			"dialog"				=> Array("base_url" => "https://accounts.google.com/o/oauth2/auth"),
+			"api"					=> Array("base_url" => "https://www.googleapis.com/oauth2/v2", "token_auth" => true),
+			"requests"				=> Array("/oauth/token" => "https://accounts.google.com/o/oauth2/token", "/oauth/token/debug" => "https://accounts.google.com/o/oauth2/token"),
+			"errors"				=> Array("throw" => true)
+		);
+		
+		// function userProfile(). Fetches the current user's profile.
+		public function userProfile() {
+			$request = $this->api("GET", "/userinfo");
+			
+			$request->execute();
+			return $request->responseObject();
+		}
+	}
+	
