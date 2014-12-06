92fec0d4d8614d6281db112b887c61615abe87c9
Samuel Elliott <samuelthomas2774@outlook.com>
2014-12-06 14:59:37 +0000

    Update microsoft.class.php
    Added dialog->scope_separator option.

--- src/microsoft.class.php
+++ src/microsoft.class.php
@@ -8,7 +8,7 @@
 		// Options. These shouldn't be modified here, but using the OAuth::options() function.
 		public $options = Array(
 			"session_prefix"		=> "microsoft_",
-			"dialog"				=> Array("base_url" => "https://login.live.com/oauth20_authorize.srf"),
+			"dialog"				=> Array("base_url" => "https://login.live.com/oauth20_authorize.srf", "scope_separator" => " "),
 			"api"					=> Array("base_url" => "https://apis.live.net/v5.0", "token_auth" => true),
 			"requests"				=> Array("/oauth/token" => "https://login.live.com/oauth20_token.srf", "/oauth/token/debug" => "https://login.live.com/oauth20_token.srf"),
 			"errors"				=> Array("throw" => true)
