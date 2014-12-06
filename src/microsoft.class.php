deb841029a932d83b3660e486d77e8028f372650
Samuel Elliott <samuelthomas2774@outlook.com>
2014-12-06 13:27:15 +0000

    Create microsoft.class.php

---
+++ src/microsoft.class.php
@@ -0,0 +1,25 @@
+<?php
+	/* class OAuthMicrosoft
+	 * /src/microsoft.class.php
+	 */
+	require_once 'oauth.class.php';
+	
+	class OAuthMicrosoft extends OAuth {
+		// Options. These shouldn't be modified here, but using the OAuth::options() function.
+		public $options = Array(
+			"session_prefix"		=> "microsoft_",
+			"dialog"				=> Array("base_url" => "https://login.live.com/oauth20_authorize.srf"),
+			"api"					=> Array("base_url" => "https://apis.live.net/v5.0", "token_auth" => true),
+			"requests"				=> Array("/oauth/token" => "https://login.live.com/oauth20_token.srf", "/oauth/token/debug" => "https://login.live.com/oauth20_token.srf"),
+			"errors"				=> Array("throw" => true)
+		);
+		
+		// function userProfile(). Fetches the current user's profile.
+		public function userProfile() {
+			$request = $this->api("GET", "/me"));
+			
+			$request->execute();
+			return $request->responseObject();
+		}
+	}
+	
