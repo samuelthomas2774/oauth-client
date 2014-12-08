f687314b544a9bca133ae068c284a1b903e0e052
Samuel Elliott <samuelthomas2774@users.noreply.github.com>
2014-12-08 13:42:18 +0000

    Update linkedin.class.php

--- src/linkedin.class.php
+++ src/linkedin.class.php
@@ -9,10 +9,7 @@
 		public $options = Array(
 			"session_prefix"		=> "linkedin_",
 			"dialog"				=> Array("base_url" => "https://www.linkedin.com/uas/oauth2/authorization", "scope_separator" => " "),
-			"api"					=> Array("base_url" => "https://api.linkedin.com/v1", "token_auth" => 2, "headers" => Array(
-				"User-Agent"			=> "OAuth 2.0 Client https://github.com/samuelthomas2774/oauth-client",
-				"X-Li-Format"			=> "json"
-			), "callback" => null),
+			"api"					=> Array("base_url" => "https://api.linkedin.com/v1", "token_auth" => 2, "headers" => Array("User-Agent" => "OAuth 2.0 Client https://github.com/samuelthomas2774/oauth-client", "X-Li-Format" => "json"), "callback" => null),
 			"requests"				=> Array("/oauth/token" => "https://www.linkedin.com/uas/oauth2/accessToken", "/oauth/token:response" => "json", "/oauth/token/debug" => "https://www.linkedin.com/uas/oauth2/accessToken"),
 			"errors"				=> Array("throw" => true)
 		);
@@ -22,8 +19,7 @@
 			$request = $this->api("GET", "/people/~");
 			
 			$request->execute();
-			return $response = $request->response();
-			return json_decode(json_encode(simplexml_load_string($response)), false);
+			return $request->responseObject();
 		}
 	}
 	
