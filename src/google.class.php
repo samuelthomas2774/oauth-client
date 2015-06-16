1f8293298522d57dec6f27eb7dfd79060eecab79
Samuel Elliott <samuelthomas2774@users.noreply.github.com>
2015-06-16 21:24:44 +0100

    Update google.class.php
    Fixed missing api->callback option.

--- src/google.class.php
+++ src/google.class.php
@@ -9,7 +9,7 @@
 		public $options = Array(
 			"session_prefix"		=> "google_",
 			"dialog"				=> Array("base_url" => "https://accounts.google.com/o/oauth2/auth", "scope_separator" => " "),
-			"api"					=> Array("base_url" => "https://www.googleapis.com/oauth2/v2", "token_auth" => true),
+			"api"					=> Array("base_url" => "https://www.googleapis.com/oauth2/v2", "token_auth" => true, "callback" => null),
 			"requests"				=> Array("/oauth/token" => "https://accounts.google.com/o/oauth2/token", "/oauth/token/debug" => "https://accounts.google.com/o/oauth2/token"),
 			"errors"				=> Array("throw" => true)
 		);
