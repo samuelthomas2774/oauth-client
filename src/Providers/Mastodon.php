e489a05d1e47f06f34ac74e0eaa5de4dca661d0e
Samuel Elliott <samuel@fancy.org.uk>
2018-09-26 22:10:29 +0100

    Add Mastodon

---
+++ src/Providers/Mastodon.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace OAuth2\Providers;
+
+use OAuth2\OAuth;
+use OAuth2\AccessToken;
+use OAuth2\UserProfilesInterface;
+use OAuth2\UserPicturesInterface;
+
+class Mastodon extends OAuth implements UserProfilesInterface, UserPicturesInterface
+{
+    /**
+     * Session prefix.
+     *
+     * @var string
+     */
+    public $session_prefix = 'mastodon_';
+
+    /**
+     * Base API URL.
+     *
+     * @var string
+     */
+    public $base_api_endpoint = 'https://mastodon.social';
+
+    /**
+     * OAuth 2.0 authorise endpoint.
+     *
+     * @var string
+     */
+    public $authorise_endpoint = 'https://mastodon.social/oauth/authorize';
+
+    /**
+     * OAuth 2.0 token endpoint.
+     *
+     * @var string
+     */
+    public $token_endpoint = 'https://mastodon.social/oauth/token';
+
+    /**
+     * Creates a new OAuth client object.
+     *
+     * @param string $client_id
+     * @param string $client_secret
+     * @param \OAuth2\AccessToken|string $access_token
+     * @param array $options
+     */
+    public function __construct(string $client_id, string $client_secret, $token = null, array $options = [])
+    {
+        if (array_key_exists('instance_url', $options)) {
+            $this->setInstanceUrl($options['instance_url']);
+        }
+
+        parent::__construct($client_id, $client_secret, $token, $options);
+    }
+
+    public function setInstanceUrl(string $instance_url)
+    {
+        $instance_url = rtrim($instance_url, '/');
+
+        $this->session_prefix = 'mastodon_' . preg_replace(['/[^a-z0-9-_.]/i', '/[-.]/', '/(.){2,}/'], ['', '_', '$1'], $instance_url) . '_';
+        $this->base_api_endpoint = $instance_url . '/api/v1/';
+        $this->authorise_endpoint = $instance_url . '/oauth/authorize';
+        $this->token_endpoint = $instance_url . '/oauth/token';
+    }
+
+    /**
+     * Returns the current user.
+     *
+     * @return \stdClass
+     */
+    public function getUserProfile()
+    {
+        $response = $this->api('GET', 'account/verify_credentials');
+
+        $user = new stdClass();
+        $user->id = isset($response->id) ? $response->id : null;
+        $user->username = isset($response->username) ? $response->username : $user->id;
+        $user->name = isset($response->display_name) ? $response->display_name : $user->username;
+        $user->email = isset($response->email) ? $response->email : null;
+        $user->response = $response;
+
+        return $user;
+    }
+
+    /**
+     * Returns the URL of the current user's picture.
+     *
+     * @return string
+     */
+    public function getUserPictureUrl(int $size = 50): string
+    {
+        $response = $this->api('GET', 'account/verify_credentials');
+
+        if (!isset($response->avatar)) return;
+
+        return $response->avatar;
+    }
+}
